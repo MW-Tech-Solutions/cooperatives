77f8a6119d0633dbb8255f4e1d7f1a7703a5f511
Muhammad Adamu Garba <muhdmukhtar2019@gmail.com>
2026-04-23 16:07:26 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
 'use client';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCa1mcuvbxnC7IYKZGbituHDRUXoYwe8hQ",
-  authDomain: "pyroutes-88ros.firebaseapp.com",
-  databaseURL: "https://pyroutes-88ros-default-rtdb.firebaseio.com",
-  projectId: "pyroutes-88ros",
-  storageBucket: "pyroutes-88ros.firebasestorage.app",
-  messagingSenderId: "967827192083",
-  appId: "1:967827192083:web:edc4ad6f032466e75702e6"
+  apiKey: "AIzaSyAC7wEZ1yxwi1M60Z-0S733QDfOFPMJP8E",
+  authDomain: "cooperativesociety.firebaseapp.com",
+  projectId: "cooperativesociety",
+  storageBucket: "cooperativesociety.firebasestorage.app",
+  messagingSenderId: "497694162689",
+  appId: "1:497694162689:web:e5ccb619c700858575a500",
+  measurementId: "G-GF7JNZR9P4"
 };
